2f9d9e0dc5404e80c642290faf18f65741cb8490
Bintang Muhammad <bintangmhd@gmail.com>
2015-05-11 18:45:28 +0700

    Index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>SMA Negeri 9 Yogyakarta</title>
+
+		<!-- Bootstrap CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="fluid header">
+			<div class="col-md-push-6 col-lg-push-6 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+				<h1>SMA Negeri 9 Yogyakarta</h1>
+				Jalan Sagan 1, Yogyakarta
+			</div>	
+		</div>
+		<div class="col-md-push-8 col-lg-push-8 container col-xs-12 col-sm-12 col-md-4 col-lg-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					Login
+				</div>
+				<div class="panel-body">
+				   <form action="" method="POST" role="form">		   
+				   	<div class="form-group">
+				   		<label for="user">Username</label>
+				   		<input type="text" class="form-control" id="user" placeholder="Input field">
+				   	</div>
+				   	<div class="form-group">
+				   		<label for="pass">Password</label>
+				   		<input type="password" class="form-control" id="pass" placeholder="Input field">
+				   	</div>
+				   
+				   	
+				   
+				   	<button type="submit" class="btn btn-primary">Submit</button>
+				   </form>
+				</div>
+			</div>
+		</div>
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
